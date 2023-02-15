86cdacee8d802647865b8efd2b8baef629d5df57
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-02-15 17:43:30 -0600

    se terminó de ver el tutorial de typescript desde cero

---
+++ calculator.ts
@@ -0,0 +1,14 @@
+type Operation = 'multiply' | 'add' | 'divide'
+type Result = number
+const calculator = (a:number, b:number, op: Operation): Result =>{
+   
+    if(op === 'multiply') return a * b
+    if(op === 'add') return a + b
+    if(op === 'divide'){
+        if(b === 0) throw new Error('Cant divide by 0!')
+        return a / b 
+    }
+    throw new Error('Operation is not valid')
+}
+
+console.log(process.argv)
